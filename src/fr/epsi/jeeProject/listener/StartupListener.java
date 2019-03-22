27db30a4656ecfcb13b1095d68c762a82a51aa19
aitylies <aitylies@hotmail.fr>
2019-03-22 11:54:38 +0100

    Initial commit

---
+++ src/fr/epsi/jeeProject/listener/StartupListener.java
@@ -0,0 +1,44 @@
+package fr.epsi.jeeProject.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+/**
+ * Application Lifecycle Listener implementation class StartupListener
+ *
+ */
+@WebListener
+public class StartupListener implements ServletContextListener {
+
+	private static final Logger Logger = LogManager.getLogger(StartupListener.class); 
+	
+    /**
+     * Default constructor. 
+     */
+    public StartupListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         System.out.println("Démarrage");
+         Logger.debug("Démarrage");
+         //Logger.info("Démarrage");
+    }
+	
+}
